5d80a4b36e0abd47ae4c182e5504c474f84d6c6a
mrLadejobi <ladejobioluwasaanumi@gmail.com>
2026-01-29 13:54:47 +0100

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForms
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForms(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForms()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        # instance=post tells Django to update the existing record
+        form = PostForms(request.POST, instance=post) 
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForms(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
